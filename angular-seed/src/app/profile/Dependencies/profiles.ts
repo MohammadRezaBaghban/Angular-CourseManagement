3a8026c911573bc7df01bfa6b398465d0b2c7224
MohammadBaghban <Mohammad.Baghban@HighTechICT.com>
2020-03-15 23:30:21 +0100

    Refine whole application layout into tables + Refine CSS

--- angular-seed/src/app/profile/Dependencies/profiles.ts
+++ angular-seed/src/app/profile/Dependencies/profiles.ts
@@ -3,8 +3,8 @@ import { User } from 'src/app/user';
 
 export let Profiles: Array<Profile> = [
     new Profile('Software Engineering', 'S', new User(101, 'CourseOwner', "Mike", "Nebreska", '14-11-1985')),
-    new Profile('Media Design', 'M', new User(102, 'CourseOwner', "Adam", "Bert", '15-11-1985')),
     new Profile('Embedded Systems', 'T', new User(103, 'CourseOwner', "Jason", "Kaas", '14-11-1985')),
+    new Profile('Media Design', 'M', new User(102, 'CourseOwner', "Adam", "Bert", '15-11-1985')),
     new Profile('IT', 'B', new User(104, 'CourseOwner', "Ruud", "Tiger", '14-11-1985')),
 
 
