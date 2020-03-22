a01c6035217d85ad6ba415ba8183694b10f25309
Abdullah <A.alkhalaf@student.fontys.nl>
2020-03-22 19:22:39 +0100

    Implemented a new main-nav component from Angular material library

--- angular-seed/src/app/app.module.ts
+++ angular-seed/src/app/app.module.ts
@@ -10,6 +10,14 @@ import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
 import { ProfileComponent } from './Components/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './Components/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -18,14 +26,22 @@ import { ProfileComponent } from './Components/profile/profile.component';
     RoleComponent,
     UsersComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularMultiSelectModule
+    AngularMultiSelectModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
